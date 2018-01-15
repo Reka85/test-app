80005dc530d6f0e00d9844e27cda4626bf73ee63
Reka Kulinyi <reekaa85@gmail.com>
2018-01-15 19:36:28 +0100

    price tests

--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Price, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'ensures currency presence' do
+    user = User.new(username: "Joe")
+    product = Product.new(name: "adidas", user: user)
+    price = Price.new(currency: "", value: "54")
+    expect(price).to_not be_valid
+  end
+
+  it 'ensures value presence' do
+    user = User.new(username: "Joe")
+    product = Product.new(name: "adidas", user: user)
+    price = Price.new(currency: "Eur", value: "")
+    expect(price).to_not be_valid
+  end
+
+  it 'ensures product presence' do
+    price = Price.new(currency: "Eur", value: "")
+    expect(price).to_not be_valid
+  end
+
+  it 'should save successfully' do
+    user = User.new(username: "Joe")
+    product = Product.new(name: "adidas", user: user)
+    price = Price.new(currency: "Eur", value: "54", product:product).save
+    expect(price).to eq(true)
+  end
 end
