a1e05d6e684272d5ff764f7b645f0978a67422a5
Reka Kulinyi <reekaa85@gmail.com>
2018-01-15 18:23:23 +0100

    user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "ensures username presence" do
+    user = User.new(username: "")
+    expect(user).to_not be_valid
+  end
+
+  it "should save successfully" do
+    user = User.new(username: "Mary").save
+    expect(user).to eq(true)
+  end
+
+  it "ensures username uniqueness" do
+    user = User.new(username: "Joe").save
+    user_new = User.new(username: "Joe")
+    expect(user_new).to_not be_valid
+  end
 end
